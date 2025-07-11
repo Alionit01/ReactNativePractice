7ca0721043d16744fef38ea92ab6de153393ebb7
Alionit01 <fotopob33@gmail.com>
2025-07-11 20:54:37 +0500

    complete stack

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native';
 
-export default function HomeScreen() {
+export default function SettingsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>🏠 Home Screen</Text>
+      <Text>⚙️ Settings Screen</Text>
     </View>
   );
 }
